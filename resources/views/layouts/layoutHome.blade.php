53b9290d562e234b4fbb8a4a4f05b4b6e8c7f9df
Sergi Virgili <svirgilif@gmail.com>
2019-09-10 14:58:59 +0200

    MENU LATERAL Y PAGINA INICIAL HEADER

--- resources/views/layouts/layoutHome.blade.php
+++ resources/views/layouts/layoutHome.blade.php
@@ -7,27 +7,70 @@
 
     <!-- Bootstrap CSS -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css">
     <title>Can Cruz - Casa Rural
     </title>
-  </head>
-  <body>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-  <header>
-        {{-- <div id="page-content-wrapper">
+    </head>
+    
+    <body>
 
-                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-                  <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
-        <div class="bg-light border-right" id="sidebar-wrapper">
-                <div class="sidebar-heading">Start Bootstrap </div>
-                <div class="list-group list-group-flush">
-                  <a href="#" class="list-group-item list-group-item-action bg-light">Sobre Nosotros</a>
-                  <a href="#" class="list-group-item list-group-item-action bg-light">Item2</a>
-                  <a href="#" class="list-group-item list-group-item-action bg-light">Contacto</a>
+            <div class="d-flex" id="wrapper">
+
+                    
+                    <!-- /#sidebar-wrapper -->
                 
-                </div>
-        </div> --}}
+                    <!-- Page Content -->
+                    <div id="page-content-wrapper">
+                
+                        <i class="fas fa-bars" id="menu-toggle"></i>
+                        
+                
+                        {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                        </button> --}}
+                
+                        
+                       
+                        <div class="main-slide"></div>
+                        <div id="logo"><img src="img/logo3.png" alt="logo can cruz casa rural"></div>
+                        @yield('content')
+                    </div>
+                    <!-- /#page-content-wrapper -->
+                    <!-- Sidebar -->
+                    <div class="bg-light border-right" id="sidebar-wrapper">
+                     
+                            <div class="list-group list-group-flush">
+                            
+                              <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+                              <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
+                              <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
+                              <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
+                              <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
+                              <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
+                            </div>
+                          </div>
+                  </div>
+                  <!-- /#wrapper -->
+                
+                  <!-- Bootstrap core JavaScript -->
+                  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+                  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+                
+                  <!-- Menu Toggle Script -->
+                  <script>
+                    $("#menu-toggle").click(function(e) {
+                      e.preventDefault();
+                      $("#wrapper").toggleClass("toggled");
+                    });
+                  </script>
+  {{-- <header>
+        
+        
   <div class="main-slide"></div>
   <div id="logo"><img src="img/logo3.png" alt="logo can cruz casa rural"></div>
   
@@ -61,5 +104,10 @@
             @yield('content')
         </main>
     </div>
+</div> --}}
+
+ 
+
+ 
 </body>
 </html>
